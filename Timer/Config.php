88c0a351506a24af2686f076f5acde2de809b5fd
husamhane <32095148+husamhane@users.noreply.github.com>
2021-02-06 22:06:58 +0200

    Add files via upload

---
+++ Timer/Config.php
@@ -0,0 +1,6 @@
+<?php
+	$GLOBALS[ "host" ] = "46.105.105.77";
+	$GLOBALS[ "username " ] = "professi_timerr";
+	$GLOBALS[ "password " ] = "timer12345";
+	$GLOBALS[ "database " ] = "professi_timer";
+?>
