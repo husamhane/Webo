9ab54eae93b21f6eae2d47fcb536ccdc78dd9e3f
husamhane <32095148+husamhane@users.noreply.github.com>
2021-02-06 22:08:27 +0200

    Update Config.php

--- Timer/Config.php
+++ Timer/Config.php
@@ -1,6 +1,6 @@
 <?php
-	$GLOBALS[ "host" ] = "46.105.105.77";
-	$GLOBALS[ "username " ] = "professi_timerr";
-	$GLOBALS[ "password " ] = "timer12345";
+	$GLOBALS[ "host" ] = "51.68.143.81";
+	$GLOBALS[ "username " ] = "7";
+	$GLOBALS[ "password " ] = "rrrvxdnp";
 	$GLOBALS[ "database " ] = "professi_timer";
 ?>
